7f5cc1ffdc5b10841adc6d2872cd4d9ce16fe94a
Aritra Sarkar <121709568+AritraSarkar03@users.noreply.github.com>
2024-08-14 06:45:54 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { getDatabase } from 'firebase/database';
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,//"AIzaSyDmF4rcZ8UP47EocBkDG32z0AJSA-RAqHo",
-    authDomain: process.env.AUTH_DOMAIN,//"virtualadda-7a947.firebaseapp.com",
-    projectId: process.env.PROJECT_ID,//"virtualadda-7a947",
-    storageBucket: process.env.STORAGE_BUCKET,//"virtualadda-7a947.appspot.com",
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,//"181275868461",
-    appId: process.env.APP_ID,//"1:181275868461:web:aef72c6b0264ef051427a4",
-    measurementId: process.env.MEASUREMENT_ID,//"G-SEGV58HFV9",
-    databaseURL: process.env.DATABASE_URL//"https://virtualadda-7a947-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+    databaseURL: process.env.DATABASE_URL
   };
 
   const app = initializeApp(firebaseConfig);
