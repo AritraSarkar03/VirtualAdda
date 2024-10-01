c417046f9a7e3782682a1ac82aa0a660c2a35f19
Aritra Sarkar <121709568+AritraSarkar03@users.noreply.github.com>
2024-10-01 07:50:00 -0700

    Update index.js 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { AuthProvider } from './Context/authContext';
+import { AuthProvider } from './Context/authContext.js';
 import reportWebVitals from './reportWebVitals.js';
 import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
 
